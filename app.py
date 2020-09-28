369936aea08e2ea7161569c09a173032b2350ee0
Govinda <gkhabas76@gmail.com>
2020-09-28 23:28:08 +0200

    shortened the url and edited some strings

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 
-url = 'https://www.amazon.de/Neu-Apple-iPhone-SE-64-GB/dp/B0875Q8CD9/ref=sr_1_1_sspa?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=iphone+se&qid=1600896020&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFMSDY0RFRWUDNUTjQmZW5jcnlwdGVkSWQ9QTAxNDQyMDA4SFFWMUgxREwwUlYmZW5jcnlwdGVkQWRJZD1BMTAwNDU1MzNLVEEwT0hYU1dHSEkmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl'
+url = 'https://rb.gy/3bnrug'
 
 headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
@@ -35,16 +35,17 @@ def send_email():
     server.starttls()
     server.ehlo()
 
-    server.login('bourbonwhiskey11@gmail.com', 'cltdzryzhireyhuq')
+    server.login('email', 'password')
 
-    subject = 'Product price has not been decreased :(!'
-    body = 'Check the amazon link https://www.amazon.de/Neu-Apple-iPhone-SE-64-GB/dp/B0875Q8CD9/ref=sr_1_1_sspa?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=iphone+se&qid=1600896020&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFMSDY0RFRWUDNUTjQmZW5jcnlwdGVkSWQ9QTAxNDQyMDA4SFFWMUgxREwwUlYmZW5jcnlwdGVkQWRJZD1BMTAwNDU1MzNLVEEwT0hYU1dHSEkmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl'
+    subject = 'Amazing !'
+    body = f'''Hey, I just want to take a moment of yours to remind you.
+            Last chance to grab this amazing product with discounted price 
+            {url}'''
 
     msg = f"Subject: {subject}\n\n {body}"
 
-    sender = 'bourbonwhiskey11@gmail.com'
-    recipients = ['khabasgovinda@gmail.com',
-                  'govkha18@gmail.com']
+    sender = 'email'
+    recipients = ['email/s']
 
     server.sendmail(
         sender,
@@ -52,7 +53,7 @@ def send_email():
         msg
     )
 
-    print('Successfull sent email')
+    print('Successfully sent email')
     server.quit()
 
 
